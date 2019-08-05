c9a914f5183c5f70963100b95102d472a060f5ae
analiviam <analimendonca@gmail.com>
2019-08-05 11:41:21 -0300

    2calcom1ogin

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-
+import {AngularFireAuth} from "angularfire2/auth";
+import {Aluno} from '../cadastroaluno/entidadealuno/aluno';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+aluno:Aluno=new Aluno();
+  constructor(private afAuth: AngularFireAuth, private router: Router) {}
+  logar(){
+  this.afAuth.auth.signInWithEmailAndPassword(this.aluno.email, this.aluno.senha).then(
+  () => { this.router.navigate(['calendarioaluno']); }
+  ).catch( (erro) => console.log(erro) );
+  }
 
-  constructor() {}
-
+  logout() {
+  this.afAuth.auth.signOut();
+  this.router.navigate(['/']);
+  }
 }
